d1f5c8a81f18ba60815addc46b07f89c34a714be
LiuAHao <2659414541@qq.com>
2025-03-30 00:54:22 -0700

    重建初步kv存储

--- src/RaftException.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdexcept>
-
-class RaftException : public std::runtime_error {
-public:
-    explicit RaftException(const std::string& message) 
-        : std::runtime_error(message) {}
-};
